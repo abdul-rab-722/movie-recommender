b6dd0050dfea63a0cb2e432e3ee2e007cab8a8be
abdul rab <rababdul5786@gmail.com>
2024-12-14 17:14:04 +0530

    path updated

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('movie-recommender/model/movie_list.pkl', 'rb'))
-similarity = pickle.load(open('movie-recommender/model/similarity.pkl', 'rb'))
+movies = pickle.load(open('model/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('model/similarity.pkl', 'rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
